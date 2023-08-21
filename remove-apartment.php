8cb0410080b7970f6ea4ea696b9322cff6e3fff4
Sriram-M-Bharadwaj <srirammbharadwaj2002@gmail.com>
2023-08-21 22:26:44 +0530

    .

---
+++ remove-apartment.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+  error_reporting(0);
+  include('includes/dbconn.php');
+
+  if (strlen($_SESSION['avmsaid']==0)) {
+    header('location:logout.php');
+    } else {
+
+if(isset($_GET['id'])){
+$id=$_GET['id'];
+
+include 'includes/dbcon.php';
+
+echo"Working";
+
+$qry="DELETE from apartment where id=$id";
+$result=mysqli_query($con,$qry);
+
+if($result){
+    echo"DELETED";
+    header('Location:manage-apartment.php');
+}else{
+    echo"ERROR!!";
+}
+}
+}
+?>
